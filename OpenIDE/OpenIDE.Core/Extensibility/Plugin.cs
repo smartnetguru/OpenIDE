2dc3df94353db54edc276544d36908361a885cd1
Chris <cmagerabronn110@web.de>
2015-10-25 21:51:58 +0100

    added splittedview

--- OpenIDE/OpenIDE.Core/Extensibility/Plugin.cs
+++ OpenIDE/OpenIDE.Core/Extensibility/Plugin.cs
@@ -73,6 +73,8 @@ namespace OpenIDE.Core.Extensibility
 
                 Dependencies.Add(l);
             }));
+
+            _engine.Add("plugin", this);
         }
 
         public static Plugin Load(string name)
---
+++ OpenIDE/OpenIDE.Core/Views/SplittedView.cs
@@ -0,0 +1,23 @@
+﻿using OpenIDE.Core.Contracts;
+using OpenIDE.Core.Extensibility;
+using System.IO;
+
+namespace OpenIDE.Core.Views
+{
+    public class SplittedView : View
+    {
+        public SplittedView(ItemTemplate p, View v)
+        {
+            var editor = EditorBuilder.Build(p.Extension, null, null, null);
+            editor.Text = new StreamReader(new MemoryStream(Data)).ReadToEnd();
+
+            v.Data = Data;
+            var first = v.GetView();
+            first.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            var c = new System.Windows.Forms.SplitContainer();
+            c.Panel1.Controls.Add(first);
+            c.Panel2.Controls.Add(editor);
+        }
+    }
+}
