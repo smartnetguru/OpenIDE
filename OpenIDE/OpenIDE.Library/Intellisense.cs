148854092ece20a3eb8c5b3a8b52e9eec0763e99
Chris <cmagerabronn110@web.de>
2015-11-17 09:22:04 +0100

    added real intellisense to js plugin + added snippets

--- OpenIDE/OpenIDE.Library/Intellisense.cs
+++ OpenIDE/OpenIDE.Library/Intellisense.cs
@@ -4,12 +4,17 @@ namespace OpenIDE.Library
 {
     public class Intellisense
     {
-        public List<DigitalRune.Windows.TextEditor.Completion.DefaultCompletionData> _data 
-            = new List<DigitalRune.Windows.TextEditor.Completion.DefaultCompletionData>();
+        public List<DigitalRune.Windows.TextEditor.Completion.ICompletionData> _data 
+            = new List<DigitalRune.Windows.TextEditor.Completion.ICompletionData>();
 
         public void Add(string title, string description, int imageindex = -1)
         {
             _data.Add(new DigitalRune.Windows.TextEditor.Completion.DefaultCompletionData(title, description, imageindex));
+        }
+        public void AddSnippet(string shortcut, string text, int imageIndex = -1)
+        {
+            _data.Add(new DigitalRune.Windows.TextEditor.Completion.SnippetCompletionData(
+                new DigitalRune.Windows.TextEditor.Completion.Snippet(shortcut, text), imageIndex));
         }
     }
 }
